30aa73a1fadff62037c4fd487db709046ec7921b
Michal Adam NTB <Michal.Adam@student.soskolin.cz>
2022-05-04 11:09:03 +0100

    StorageApp basic GUI

---
+++ StorageApp/StorageApp/StorageApp/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace StorageApp
+{
+    partial class formStorageApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bSearch = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbQuantity = new System.Windows.Forms.ComboBox();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.cbAge = new System.Windows.Forms.ComboBox();
+            this.cbColor = new System.Windows.Forms.ComboBox();
+            this.cbBoxNum = new System.Windows.Forms.ComboBox();
+            this.cbBoxColor = new System.Windows.Forms.ComboBox();
+            this.cbPrice = new System.Windows.Forms.ComboBox();
+            this.cbName = new System.Windows.Forms.ComboBox();
+            this.cbDesc = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.bSearch);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cbQuantity);
+            this.panel1.Controls.Add(this.cbType);
+            this.panel1.Controls.Add(this.cbAge);
+            this.panel1.Controls.Add(this.cbColor);
+            this.panel1.Controls.Add(this.cbBoxNum);
+            this.panel1.Controls.Add(this.cbBoxColor);
+            this.panel1.Controls.Add(this.cbPrice);
+            this.panel1.Controls.Add(this.cbName);
+            this.panel1.Controls.Add(this.cbDesc);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(149, 555);
+            this.panel1.TabIndex = 0;
+            // 
+            // bSearch
+            // 
+            this.bSearch.BackColor = System.Drawing.Color.White;
+            this.bSearch.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bSearch.FlatAppearance.BorderSize = 0;
+            this.bSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bSearch.Location = new System.Drawing.Point(0, 502);
+            this.bSearch.Name = "bSearch";
+            this.bSearch.Size = new System.Drawing.Size(149, 53);
+            this.bSearch.TabIndex = 1;
+            this.bSearch.Text = "Search";
+            this.bSearch.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(12, 262);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "QUANTITY";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "TYPE";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(12, 412);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "AGE";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(12, 362);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "COLOR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BOX COLOR";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(12, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "BOX NUMBER";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "NAME";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(12, 312);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "PRICE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DESCRIPTION";
+            // 
+            // cbQuantity
+            // 
+            this.cbQuantity.FormattingEnabled = true;
+            this.cbQuantity.Location = new System.Drawing.Point(12, 283);
+            this.cbQuantity.Name = "cbQuantity";
+            this.cbQuantity.Size = new System.Drawing.Size(121, 21);
+            this.cbQuantity.TabIndex = 0;
+            // 
+            // cbType
+            // 
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Location = new System.Drawing.Point(12, 83);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(121, 21);
+            this.cbType.TabIndex = 0;
+            // 
+            // cbAge
+            // 
+            this.cbAge.FormattingEnabled = true;
+            this.cbAge.Location = new System.Drawing.Point(12, 433);
+            this.cbAge.Name = "cbAge";
+            this.cbAge.Size = new System.Drawing.Size(121, 21);
+            this.cbAge.TabIndex = 0;
+            // 
+            // cbColor
+            // 
+            this.cbColor.FormattingEnabled = true;
+            this.cbColor.Location = new System.Drawing.Point(12, 383);
+            this.cbColor.Name = "cbColor";
+            this.cbColor.Size = new System.Drawing.Size(121, 21);
+            this.cbColor.TabIndex = 0;
+            // 
+            // cbBoxNum
+            // 
+            this.cbBoxNum.FormattingEnabled = true;
+            this.cbBoxNum.Location = new System.Drawing.Point(12, 233);
+            this.cbBoxNum.Name = "cbBoxNum";
+            this.cbBoxNum.Size = new System.Drawing.Size(121, 21);
+            this.cbBoxNum.TabIndex = 0;
+            // 
+            // cbBoxColor
+            // 
+            this.cbBoxColor.FormattingEnabled = true;
+            this.cbBoxColor.Location = new System.Drawing.Point(12, 183);
+            this.cbBoxColor.Name = "cbBoxColor";
+            this.cbBoxColor.Size = new System.Drawing.Size(121, 21);
+            this.cbBoxColor.TabIndex = 0;
+            // 
+            // cbPrice
+            // 
+            this.cbPrice.FormattingEnabled = true;
+            this.cbPrice.Location = new System.Drawing.Point(12, 333);
+            this.cbPrice.Name = "cbPrice";
+            this.cbPrice.Size = new System.Drawing.Size(121, 21);
+            this.cbPrice.TabIndex = 0;
+            // 
+            // cbName
+            // 
+            this.cbName.FormattingEnabled = true;
+            this.cbName.Location = new System.Drawing.Point(12, 33);
+            this.cbName.Name = "cbName";
+            this.cbName.Size = new System.Drawing.Size(121, 21);
+            this.cbName.TabIndex = 0;
+            // 
+            // cbDesc
+            // 
+            this.cbDesc.FormattingEnabled = true;
+            this.cbDesc.Location = new System.Drawing.Point(12, 133);
+            this.cbDesc.Name = "cbDesc";
+            this.cbDesc.Size = new System.Drawing.Size(121, 21);
+            this.cbDesc.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightSkyBlue;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.GridColor = System.Drawing.Color.LightSkyBlue;
+            this.dataGridView1.Location = new System.Drawing.Point(149, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1046, 555);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // formStorageApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(1195, 555);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "formStorageApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StorageApp";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.ComboBox cbBoxColor;
+        private System.Windows.Forms.ComboBox cbName;
+        private System.Windows.Forms.ComboBox cbDesc;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbQuantity;
+        private System.Windows.Forms.ComboBox cbAge;
+        private System.Windows.Forms.ComboBox cbColor;
+        private System.Windows.Forms.ComboBox cbBoxNum;
+        private System.Windows.Forms.ComboBox cbPrice;
+        private System.Windows.Forms.Button bSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
